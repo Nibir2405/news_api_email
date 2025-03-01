3f74f7c53671f97285bdab4037758a5ef686eb8a
Nibir2405 <nibirislam56@gmail.com>
2025-03-02 01:08:58 +0600

    Add email functionality to send news articles and refactor API request

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
 import requests
+from send_email import send_email
+
+
 
 api_key = "fcfc505613eb4fbd879d70c754743880"
 
-url = "https://newsapi.org/v2/everything?q=tesla&from=2025-01-26&sortBy=publishedAt&apiKey=fcfc505613eb4fbd879d70c754743880"
+topic = "tesla"
 
-#Make a request
+url = f"https://newsapi.org/v2/everything?q={topic}&from=2025-02-01&sortBy=publishedAt&apiKey=fcfc505613eb4fbd879d70c754743880&language=en"
+
+
+# Make a request
 request = requests.get(url)
 
-#Get data from json file
+# Get data from json file
 content = request.json()
 
-#Access the article's title and description
-for article in content["articles"]:
-    print(article["title"])
-    print(article["description"])
+
+
+# To store article's title and description
+
+email_body = "" + "Subject:Today's news" + "\n"
+# Access the article's title and description
+for article in content["articles"][:20]:
+    if article is not None:
+
+        email_body =  email_body + article["title"] + "\n" + article["description"] + "\n" + article["url"] + "\n" + "-" * 20 + "\n"
+
+send_email(message=email_body)
